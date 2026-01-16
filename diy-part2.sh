3b217f150ac5da6c5f1f74b7b406f9ac426b197c
fhtx110 <70973434+fhtx110@users.noreply.github.com>
2026-01-17 00:04:27 +0800

    Update USB 3.0 unlock and configuration settings

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# 1. 强力解锁 USB 3.0 (移除所有 MediaTek 设备的 U3 禁用掩码)
+# 1. 解锁 USB 3.0 掩码 (适用于所有 MediaTek 7981 固件)
 find target/linux/mediatek/ -name "*.dts*" | xargs sed -i 's/mediatek,u3p-dis-msk = <0x01>/mediatek,u3p-dis-msk = <0x00>/g'
 
-# 2. 修改默认 IP 为你要求的 192.168.66.1
+# 2. 修改默认网关 IP 为 192.168.66.1
 sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
-# 3. 强制开启硬件加速 (PPE) 默认开关，确保 5G 转发不卡 CPU
+# 3. 开启流量分载 (HW NAT) 默认开关
 sed -i 's/p->hw_flow_offload = false/p->hw_flow_offload = true/g' package/network/config/firewall/files/firewall.config
-sed -i 's/p->hw_flow_offload = '\''0'\''/p->hw_flow_offload = '\''1'\''/g' package/network/config/firewall/files/firewall.config
+
+# 4. (可选) 增加对 RM520N 更友好的固件名称后缀
+sed -i "s/OpenWrt /Nradio-C8-5G-USB3-$(date +%Y%m%d) /g" package/base-files/files/etc/banner
